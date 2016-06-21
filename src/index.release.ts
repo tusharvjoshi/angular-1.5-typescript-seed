90acdf7a3eb060bb6902dcad1cc76fd6ca207170
Luke DeWitt <luke.dewitt@redspace.com>
2016-06-21 14:40:02 -0300

    changed order of where app css is loaded

--- src/index.release.ts
+++ src/index.release.ts
@@ -5,15 +5,15 @@ import '@angular/router/angular1/angular_1_router'
 import 'LogUnobtrusiveExtension/dist/log-ex-unobtrusive'
 // import 'ng-stats'
 
+// app css
+import './app.scss'
+
 // app imports
 import Common from './common/common.ts'
 import Components from './components/components.ts'
 
 import {App} from './app.ts'
 import AppConfig from './app.config.ts'
-
-// app css
-import './app.scss'
 
 // top level angular module for app
 angular.module('app', [
